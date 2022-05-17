94fc8e96e97e7d9a7f75f0cc332a858a7a6351b8
sparrowlivedev <dev@sparrowlive.com>
2022-05-17 15:00:25 -0400

    Setting up options and static vars

--- plugins/livesim/src/plugin.js
+++ plugins/livesim/src/plugin.js
@@ -13,6 +13,22 @@ const BC_BASE_URL = "https://edge.api.brightcove.com/playback/v1/accounts/620085
 const videoURL = "videos/${videoId}";
 const playlistURL = "playlists/${playlistId}";
 
+var _options = {};
+var _livesimEnabled = false;
+var _startTime = 0;
+var _currentTime = new Date();
+
+function setOptions(options) {
+  if (typeof options === "object") {
+    _options["displayText"] = options["displayText"];
+    _options["preLive"] = options["preLive"];
+    _options["featureTag"] = options["featureTag"];
+    _options["postLive"] = options["postLive"];
+  } else {
+    console.log("Options object is not a proper Javscript object {} (JSON).");
+  }
+}
+
 /**
  * Function to invoke when the player is ready.
  *
@@ -55,20 +71,30 @@ const onPlayerReady = (player, options) => {
  */
 
 const livesim = function(options) {
+
+  setOptions(options);
+  console.log(_options);
+
   this.ready(() => {
     var player = this;
     console.log('player ready!', player);
+
     onPlayerReady(player, videojs.mergeOptions(defaults, options));
 
     // Load video metadata
     player.on('loadstart',function(){
       console.log('loadstart! mediainfo: ', player.mediainfo);
+      var metadata = player.mediainfo || {};
+
+      // Check for the featureTag. Without this, we don't proceed with the plugin.
+      _livesimEnabled = metadata.tags && metadata.tags.includes(_options["featureTag"]);
+      console.log("_livesimEnabled", _livesimEnabled);
+
     });
 
     // Play the video in the player
     player.on('loadedmetadata', function() {
-      console.log('loadedmetadata! mediainfo: ', player.mediainfo);
-      console.log('loadedmetadata! play video');
+      console.log('loadedmetadata! setup video');
       player.currentTime(20);
       player.play();
     });
